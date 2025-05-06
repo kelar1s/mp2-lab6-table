0a3694dbb9dbae0255bf1a675008fcf29dea430c
Anton <atabalaev21@gmail.com>
2025-05-06 23:12:17 +0300

    added delete method in balanced tree

--- Table/BalancedTreeTable.h
+++ Table/BalancedTreeTable.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "TreeTable.h"
 
-const int H_OK, H_INC, H_DEC;
+const int H_OK = 0; const int H_INC = 1; const int H_DEC = -1;
+const bool InsTN = 1; const bool DelTN = 0;
 
 template <typename TKey, typename TVal>
 class BalancedTreeTable : public TreeTable<TKey, TVal> {
 protected:
-	int InsertBalancedTree(TreeNode<TKey, TVal>*& pNode, Record<TKey, TVal> rec) { //по ссылке корень тк он может измениться
+	int InsertBalancedTreeNode(TreeNode<TKey, TVal>*& pNode, Record<TKey, TVal> rec) { //по ссылке корень тк он может измениться
 		int res = H_OK;
 		if (pNode == nullptr) {
 			eff++;
@@ -16,14 +17,14 @@ protected:
 		}
 		else if (pNode->rec.key > rec.key) {
 			eff++;
-			int tmp = InsertBalancedTree(pNode->pLeft, rec);
+			int tmp = InsertBalancedTreeNode(pNode->pLeft, rec);
 			if (tmp == H_INC) {
 				res = BalanceTreeLeft(pNode);
 			}
 		}
 		else if (pNode->rec.key < rec.key) {
 			eff++;
-			int tmp = InsertBalancedTree(pNode->pRight, rec);
+			int tmp = InsertBalancedTreeNode(pNode->pRight, rec);
 			if (tmp == H_INC) {
 				res = BalanceTreeRight(pNode);
 			}
@@ -31,7 +32,7 @@ protected:
 		return res;
 	}
 
-	int BalanceTreeLeft(TreeNode<TKey, TVal>*& pNode) {
+	int BalanceTreeLeft(TreeNode<TKey, TVal>*& pNode, bool flag = InsTN) {
 		int res = H_OK;
 		if (pNode->bal == BAL_RIGHT) {
 			pNode->bal = BAL_OK;
@@ -75,14 +76,126 @@ protected:
 		return res;
 	}
 
-	int BalanceTreeRight(TreeNode<TKey, TVal>*& pNode) {
-
+	int BalanceTreeRight(TreeNode<TKey, TVal>*& pNode, bool flag = InsTN) {
+		
 	}
+
+	int DeleteBalancedTreeNode(TreeNode<TKey, TVal>*& pNode, TKey key) {
+		int res = H_OK;
+		eff++;
+		if (pNode == nullptr) {
+			return res;
+		}
+		else if (key > pNode->rec.key) {
+			int tmp = DeleteBalancedTreeNode(pNode->pRight, key);
+			if (tmp == H_DEC) {
+				res = BalanceTreeLeft(pNode, DelTN);
+			}
+		}
+		else if (key < pNode->rec.key) {
+			int tmp = DeleteBalancedTreeNode(pNode->pLeft, key);
+			if (tmp == H_DEC) {
+				res = BalanceTreeRight(pNode, DelTN);
+			}
+		}
+		else {
+			dataCount--;
+			if (pNode->pLeft == nullptr && pNode->pRight == nullptr) {
+				delete pNode;
+				pNode = nullptr;
+				res = H_DEC;
+			}
+			else if (pNode->pLeft == nullptr && pNode->pRight != nullptr) {
+				pNode->rec = pNode->pRight->rec;
+				delete pNode->pRight;
+				pNode->pRight = nullptr;
+				pNode->bal = BAL_OK;
+				res = H_DEC;
+			}
+			else if (pNode->pRight == nullptr && pNode->pLeft != nullptr) {
+				pNode->rec = pNode->pLeft->rec;
+				delete pNode->pLeft;
+				pNode->pLeft = nullptr;
+				pNode->bal = BAL_OK;
+				res = H_DEC;
+			}
+			else {
+				TreeNode<TKey, TVal> left = pNode->pLeft;
+				TreeNode<TKey, TVal> right = pNode->pRight;
+				TreeNode<TKey, TVal> minik = FindMin(right);
+				res = RemoveMin(right);
+				pNode->rec = minik->rec;
+				delete minik;
+				pNode->pLeft = left;
+				pNode->pRight = right;
+				if (res != H_OK) {
+					res = BalanceTreeLeft(pNode, DelTN);
+				}
+			}
+		}
+		return res;
+	}
+
+	TreeNode<TKey, TVal>* FindMin(TreeNode<TKey, TVal>* pNode) {
+		TreeNode<TKey, TVal>* current = pNode;
+		while (current->left != nullptr) {
+			current = current->left;
+		}
+		return current;
+	}
+
+	int RemoveMin(TreeNode<TKey, TVal>*& pNode) {
+		int res = H_OK;
+		if (pNode->pLeft == nullptr) {
+			pNode = pNode->pRight;
+			res = H_DEC;
+		}
+		else {
+			res = RemoveMin(pNode->pLeft);
+			if (res != H_OK) {
+				res = BalanceTreeRight(pNode, DelTN);
+			}
+		}
+		return res;
+	}
+
 public:
+	bool IsEmpty() {
+		return pRoot == nullptr;
+	}
+
+	bool Find(TKey key) {
+		pCurr = pRoot;
+		while (pCurr != nullptr) {
+			eff++;
+			if (key > pCurr->rec.key) {
+				pPrev = pCurr;
+				pCurr = pCurr->pRight;
+			}
+			else if (key < pCurr->rec.key) {
+				pPrev = pCurr;
+				pCurr = pCurr->pLeft;
+			}
+			else if (key == pCurr->rec.key) {
+				return true;
+			}
+		}
+		eff++;
+		pCurr = pPrev;
+		return false;
+	}
+
 	void Insert(Record<TKey, TVal> rec) {
-		if (Find(rec.key)) {
+		if (Find(rec.key) || IsEmpty()) {
 			throw - 1;
 		}
-		InsertBalancedTree(pRoot, rec);
+		InsertBalancedTreeNode(pRoot, rec);
+	}
+
+	void Delete(TKey key) {
+		if (!Find(key)) {
+			throw - 1;
+		}
+		DeleteBalancedTreeNode(pRoot, key);
 	}
 };
